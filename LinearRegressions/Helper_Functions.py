44d573a78da49597d5a5b9dfd723a83a29be10c6
schlenkerc18 <schlenkerc18@hanover.edu>
2021-06-30 13:18:58 -0400

    Refactored Helper_Functions

--- LinearRegressions/Helper_Functions.py
+++ LinearRegressions/Helper_Functions.py
@@ -37,25 +37,18 @@ def get_player_team(string):
 # function returns the player without the team
 def get_player(string):
     end = string.find('(')
-    return string[0:end]
+    return string[0:end-1]
     
 # function that returns dictionary of players with their respective fantasy points
 # for that season, only includes players who scored points
 def get_player_season(df):
-    my_dict = {df['PLAYER'].iloc[i]: [df['FPTS/G'].iloc[i]] for i in range(len(df))
+    my_dict = {get_player(df['PLAYER'].iloc[i]): [df['FPTS/G'].iloc[i]] for i in range(len(df))
            if df['FPTS/G'].iloc[i] > 0}
     return my_dict
 
 # function takes a given dictionary and adds 2nd season to a player if they
 # are already in the dictionary
 def add_season(my_dict, df):
-    for i in range(len(df)):
-        if df['PLAYER'].iloc[i] in my_dict:
-            my_dict[df['PLAYER'].iloc[i]].append(df['FPTS/G'].iloc[i])
-    return my_dict
-
-# same as add_season function, but gets string of player without team
-def add_season_2(my_dict, df):
     for i in range(len(df)):
         if get_player(df['PLAYER'].iloc[i]) in my_dict:
             my_dict[get_player(df['PLAYER'].iloc[i])].append(df['FPTS/G'].iloc[i])
