76730e7753b320b250c289454a7f2bcd48596839
schlenkerc18 <schlenkerc18@hanover.edu>
2021-07-18 15:34:34 -0400

    Added visualizations for 2019 and 2020 qb stats

--- LinearRegressions/Data_Viz.py
+++ LinearRegressions/Data_Viz.py
@@ -20,12 +20,31 @@ qb_df_2018 = pd.read_csv("C:/Users/Schlenker18/Documents/GitHub/2021-Fantasy-Foo
 qb_df_2019 = pd.read_csv("C:/Users/Schlenker18/Documents/GitHub/2021-Fantasy-Football-Rankings/WebScrapers/QbStats2019.csv")
 qb_df_2020 = pd.read_csv("C:/Users/Schlenker18/Documents/GitHub/2021-Fantasy-Football-Rankings/WebScrapers/QbStats2020.csv")
 
-# removing outliers
-# creating histogram to see distribution of data
+# creating histogram to see distribution of data, 2018 stats
 sns.set_style("white")
 plt.figure(figsize = (12,8))
-plt.hist(qb_df_2018['FPTS/G'].iloc[0:41], bins = 8, color = '#4F69C6')
+plt.hist(qb_df_2018['FPTS/G'].iloc[0:41], bins = 8, color = '#4F69C6') # removing outliers
 plt.title("Distribution of QB Fpts/G in 2018", fontsize = 14, weight = 'bold')
+plt.xlabel('Fpts/G', fontsize = 12, weight = 'bold')
+plt.ylabel('Number of QBs', fontsize = 12, weight = 'bold')
+sns.despine()
+plt.show()
+
+# creating histogram to see distribution of 2019 stats
+sns.set_style("white")
+plt.figure(figsize = (12,8))
+plt.hist(qb_df_2019['FPTS/G'].iloc[0:42], bins = 8, color = '#4F69C6') # removing outliers
+plt.title("Distribution of QB Fpts/G in 2019", fontsize = 14, weight = 'bold')
+plt.xlabel('Fpts/G', fontsize = 12, weight = 'bold')
+plt.ylabel('Number of QBs', fontsize = 12, weight = 'bold')
+sns.despine()
+plt.show()
+
+# creating histogram to see distribution of 2020 stats
+sns.set_style("white")
+plt.figure(figsize = (12,8))
+plt.hist(qb_df_2020['FPTS/G'].iloc[0:47], bins = 8, color = '#4F69C6') # removing outliers
+plt.title("Distribution of QB Fpts/G in 2020", fontsize = 14, weight = 'bold')
 plt.xlabel('Fpts/G', fontsize = 12, weight = 'bold')
 plt.ylabel('Number of QBs', fontsize = 12, weight = 'bold')
 sns.despine()
